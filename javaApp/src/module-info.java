296d628d9848f567f5501b987025bbe2eebea123
Roopa <roopa.konaparthi@gmail.com>
2022-09-21 22:05:17 +0530

    updated module.java file

--- javaApp/src/module-info.java
+++ javaApp/src/module-info.java
@@ -1,9 +1,18 @@
-/**
- * 
- */
-/**
- * @author admin
- *
- */
-module javaApp {
+package ex.inhertiance;
+
+import java.sql.Timestamp;
+
+public class A {
+	
+	int x=10;
+	A(){
+		System.out.println("Inside A Constructor x = "+x);
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		System.out.println("Inside A Constructor Time = "+timestamp);
+	}
+	
+	public void m1() {
+		
+	}
+
 }
